6a8ea084a69e3f42bb96e8387a3e4f22a36ce6ad
Igor Alekhnovych <igoraleh@gmail.com>
2021-03-17 11:48:03 +0000

    Google Sign In, Remember User

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import '../components/menu.dart';
 import 'components/sign_in.dart';
 
@@ -9,258 +9,89 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  // start comment here
+
+  bool loading = false;
+  bool isLoggedIn = false;
+
+  void onLoginStatusChanged(bool isLoggedIn) {
+    setState(() {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    isSignedIn();
+  }
+
+  void isSignedIn() async {
+    setState(() {
+      loading = true;
+    });
+
+    isLoggedIn = await googleSignIn.isSignedIn();
+
+    if (isLoggedIn) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Menu()));
+    }
+
+    setState(() {
+      loading = false;
+    });
+  }
+
+  // end comment here
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white,
+        color: Colors.amber[800],
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlutterLogo(size: 150),
+              Image(image: AssetImage('assets/icon.png'), height: 225),
               SizedBox(height: 50),
-              _signInButton(),
+              SignInButton(
+                Buttons.Google,
+                text: "Continue with Google",
+                onPressed: () {
+                  signInWithGoogle().then((result) {
+                    if (result != null) {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return Menu();
+                          },
+                        ),
+                      );
+                    }
+                  });
+                },
+              ),
+              SizedBox(height: 15),
+              SignInButton(
+                Buttons.Email,
+                text: "Continue with Mail",
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return Menu();
+                      },
+                    ),
+                  );
+                },
+              )
             ],
           ),
         ),
       ),
     );
   }
-
-  Widget _signInButton() {
-    return OutlineButton(
-      splashColor: Colors.grey,
-      onPressed: () {
-        signInWithGoogle().then((result) {
-          if (result != null) {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) {
-                  return Menu();
-                },
-              ),
-            );
-          }
-        });
-      },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-      highlightElevation: 0,
-      borderSide: BorderSide(color: Colors.grey),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                'Sign in with Google',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
 }
-
-class FirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(color: Colors.blue[100]),
-    );
-  }
-}
-
-/*
-
-class Login extends StatefulWidget {
-  bool obscureText = true;
-  IconData passIcon = Icons.remove_red_eye_outlined;
-
-  @override
-  _LoginState createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            color: Colors.white,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 40.0,
-                    ),
-                  ),
-                ),
-                Expanded(
-                    flex: 9,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Username",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextField(
-                          //Change the search variable when typing
-                          onChanged: (String val) async {
-                            setState(() {
-                              //search = val;
-                            });
-                          },
-                          //The input styling
-                          decoration: InputDecoration(
-                            //Placeholder text
-                            hintText: "Username",
-                            //The magnifying glass icon
-                            prefixIcon: Icon(
-                              Icons.person,
-                              color: Colors.black,
-                            ),
-                            //The clear search icon
-                            suffixIcon: IconButton(
-                              icon: Icon(Icons.clear),
-                              onPressed: () {
-                                //_controller.clear();
-                                //Clear search on click
-                                setState(() {
-                                  //search = "";
-                                });
-                              },
-                            ),
-                            //Background colour = true
-                            filled: true,
-                            //Background colour
-                            fillColor: Colors.grey[300],
-                            contentPadding: EdgeInsets.all(0.0),
-                            //Border when not focused
-                            enabledBorder: OutlineInputBorder(
-                              //Border colour
-                              borderSide: BorderSide(color: Colors.transparent),
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                            //Border when focused
-                            focusedBorder: OutlineInputBorder(
-                              //Border colour
-                              borderSide: BorderSide(color: Colors.transparent),
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20.0,
-                        ),
-                        Text(
-                          "Password",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10.0,
-                        ),
-                        TextField(
-                          onChanged: (String val) async {
-                            setState(() {
-                              //search = val;
-                            });
-                          },
-                          obscureText: widget.obscureText,
-                          enableSuggestions: false,
-                          autocorrect: false,
-                          //The input styling
-                          decoration: InputDecoration(
-                            //Placeholder text
-                            hintText: "Password",
-                            prefixIcon: Icon(
-                              Icons.lock,
-                              color: Colors.black,
-                            ),
-                            //The clear search icon
-                            suffixIcon: IconButton(
-                              icon: Icon(widget.passIcon),
-                              onPressed: () {
-                                //_controller.clear();
-                                setState(() {
-                                  widget.obscureText = !widget.obscureText;
-                                  widget.passIcon = widget.obscureText
-                                      ? Icons.remove_red_eye_outlined
-                                      : Icons.remove_red_eye;
-                                });
-                              },
-                            ),
-                            //Background colour = true
-                            filled: true,
-                            //Background colour
-                            fillColor: Colors.grey[300],
-                            contentPadding: EdgeInsets.all(0.0),
-                            //Border when not focused
-                            enabledBorder: OutlineInputBorder(
-                              //Border colour
-                              borderSide: BorderSide(color: Colors.transparent),
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                            //Border when focused
-                            focusedBorder: OutlineInputBorder(
-                              //Border colour
-                              borderSide: BorderSide(color: Colors.transparent),
-                              borderRadius: BorderRadius.circular(15.0),
-                            ),
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            ElevatedButton(
-                              onPressed: () async {
-                                await Navigator.pushNamed(context, "/");
-                              },
-                              child: Text("Signup"),
-                            ),
-                            SizedBox(
-                              width: 10.0,
-                            ),
-                            ElevatedButton(
-                              onPressed: () async {
-                                await Navigator.pushNamed(context, "/");
-                              },
-                              child: Text("Login"),
-                            ),
-                          ],
-                        )
-                      ],
-                    )),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-*/
