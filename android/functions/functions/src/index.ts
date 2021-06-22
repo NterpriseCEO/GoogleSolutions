04e0bafc77bafa9d414c10df02536d9e8ebb5d43
Alec K <ajkeane2414@gmail.com>
2021-06-23 00:24:14 +0100

    Added extra cloud functionality for the new collection in the DB

--- android/functions/functions/src/index.ts
+++ android/functions/functions/src/index.ts
@@ -34,6 +34,7 @@ export const expiryDateChecker = functions.pubsub.schedule("0 06 * * *")
             amountTomorrow++;
           } else if (diffInDays < 0) {
             amountExpired++;
+            db.collection(`expiryGroups/Users/${collection}`).doc(doc.id).update({Expired: "true"});
           }
         });
         if (amountToday > 0) {
