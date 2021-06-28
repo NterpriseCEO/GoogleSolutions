5771a2a1d656ab6b32db3cd4b7966b8955d05acd
Alec K <ajkeane2414@gmail.com>
2021-06-28 23:04:08 +0100

    Updated food facts

--- lib/pages/Dashboard.dart
+++ lib/pages/Dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:best_before_app/components/InventoryCard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 import '../UpdateDatabase.dart';
 import 'components/ExpiryChart.dart';
@@ -34,6 +35,34 @@ class _DashboardState extends State<Dashboard> {
     expiredAmount = 0;
     total = 0;
   }
+
+  String getRandomElement(List li){
+    final rand = new Random();
+    var i = rand.nextInt(li.length);
+    return li[i];
+  }
+  List facts = [
+    "More then 1/3 of all food is wasted. That's around 1.3 billion tonnes a year.",
+    "Food waste causes 3.3 billion tonnes of C02 emissions a year.",
+    "Almost \$1 trillion in food is wasted globally a year. Don't waste money overbuying.",
+    "An area larger then China is used to grow food that is never eaten.",
+    "25% of the worlds fresh water is used to grow food that is never eaten.",
+    "Food waste would be the 3rd largest emitter of C02 if it were a country.",
+    "In developed countries over half of total food waste is in homes rather then in restaurants.",
+    "Reducing food wastage is one of the best ways to fight climate change.",
+    "Best before dates can still be eaten after the date labelled on the package.",
+    "Eat other foods, 75% of food generated is by 12 plants and 5 animal species.",
+    "The global food system is responsible for almost 30% of all Green House Gas emissions.",
+    "33% of fish stocks are overfished at an unsustainable level leading to decreasing fish populations.",
+    "Fruit and Vegetables are the most wasted food group. Accounting for 40-50% together",
+    "25% of food waste can feed 870 million people. This would feed the 690 million underfed people.",
+    "Food rot from wasted food releases Methane which is 28x stronger then C02.",
+    "8% of Green House Gasses are caused by food waste when they rot in landfills.",
+    "One burger uses the same amount of water as a 90 minute shower. That's 2400 litres of water.",
+    "It takes 25 years for a head of lettuce to decompose in a landfill.",
+    "Large amounts of food are wasted due to appearance as people don't want to eat them.",
+    "Reducing food waste is the third most effective way to address climate change."
+  ];
 
   Future<int> getCards() async {
     List<String> categories = [
@@ -73,6 +102,8 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
+    //text element for food facts
+    var textElement = getRandomElement(facts);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SafeArea(
@@ -178,7 +209,7 @@ class _DashboardState extends State<Dashboard> {
                             ),
                           ),
                           Text(
-                            '25% of the world’s fresh water supply is used to grow food that is never eaten.',
+                            '$textElement',
                             textAlign: TextAlign.justify,
                             style: TextStyle(
                               height: 1.5,
